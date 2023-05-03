e155da33df351f5c5a27e934bfc8cf149859ed37
JLDaquillana <128910260+JLDaquillana@users.noreply.github.com>
2023-05-03 16:43:30 +0800

    Update ApolloProject.c

--- ApolloProject.c
+++ ApolloProject.c
@@ -44,6 +44,24 @@ int isProdCodeAvail(char code[]) {
     return 0;
 }
 
+// Checks if the product name is available.
+
+int isProdNameAvail(char name[]) {
+    FILE *file;
+    file = fopen("Inventory.txt", "r");
+    while (fread(&item, sizeof(item), 1, file)) {
+        int i = 0;
+        while (name[i] != '\0' && item.product_name[i] != '\0' && name[i] == item.product_name[i]) {
+            i++;
+        } if (name[i] == '\0' && item.product_name[i] == '\0') {
+            fclose(file);
+            return 1;
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
 // Checks if the input is an integer.
 int getinteger(int input) {
     int result = scanf("%d", &input);
@@ -154,6 +172,138 @@ void addItem() {
     }
 }
 
+// UPDATE ITEMS
+// updates and/or deletes an item on the data file.
+void update() {
+    printf("\tUpdate Product\n");
+    printf("************************\n");
+
+    FILE *file1, *file2;
+    char code[20], product[20];
+    int available, choice, productCount = 0;
+
+    file1 = fopen("Inventory.txt", "rb");
+    if (file1 != NULL) {
+        while (fread(&item, sizeof(item), 1, file1)) {
+            productCount++;
+        }
+        fclose(file1);
+    }
+
+    if (productCount == 0) {
+        printf("\nThere are no products listed. Please add an item first.\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    do {
+        system("cls");
+        int option;
+
+        printf("Choose an option: \n");
+        printf("1. Display Inventory\n");
+        printf("2. Search for Product by Name\n");
+        printf("3. Go back to main interface.\n");
+        scanf("%d", &option);
+
+
+        if (option == 1) {
+            display();
+            printf("\nEnter the Product code to update or delete from the Inventory: ");
+            scanf("%s", code);
+            available = isProdCodeAvail(code);
+        } else if (option == 2) {
+            printf("\nEnter the Product name to search: ");
+            scanf("%s", product);
+            available = isProdNameAvail(product);
+        } else if (option == 3) {
+            system("cls");
+            return;
+        } else {
+            printf("\nInvalid option. Please try again.\n");
+            return;
+        }
+
+        if (available == 0) {
+            printf("\nNo product found for update or delete.\n");
+            printf("Press 1 to try again, or any other key to go back to the main menu.\n");
+            int retryChoice;
+            scanf("%d", &retryChoice);
+            if (retryChoice != 1) {
+                return;
+            }
+        } else {
+            do {
+                printf("\nWhat do you want to do with the product?\n");
+                printf("1. Update\n");
+                printf("2. Delete\n");
+                printf("Enter your choice (1-2): ");
+                scanf("%d", &choice);
+            } while (choice != 1 && choice !=2);
+
+            file1 = fopen("Inventory.txt", "rb");
+            file2 = fopen("tempfile.txt", "wb");
+
+            while (fread(&item, sizeof(item), 1, file1)) {
+                int i = 0;
+                while (code[i] != '\0' && item.product_code[i] != '\0' && code[i] == item.product_code[i]) {
+                    i++;
+                }
+
+                if (code[i] == '\0' && item.product_code[i] == '\0') {
+                    if (choice == 1) {
+                        printf("\nUpdating Data of the chosen product. %s\n", code);
+
+                        printf("Enter Product Name: ");
+                        scanf("%s", item.product_name);
+
+                        printf("Enter Product price: ");
+                        scanf("%d", &item.price);
+
+                        printf("Enter Quantity: ");
+                        scanf("%d", &item.quantity);
+
+                        printf("\n");
+
+                        fwrite(&item, sizeof(item), 1, file2);
+                    }
+                    else if (choice == 2) {
+                        printf("\nDeleting Product. %s\n", code);
+                    }
+                } else {
+                    fwrite(&item, sizeof(item), 1, file2);
+                }
+            }
+
+            fclose(file1);
+            fclose(file2);
+
+            if (choice == 2) {
+                remove("Inventory.txt");
+                rename("tempfile.txt", "Inventory.txt");
+            } else {
+                file1 = fopen("Inventory.txt", "wb");
+                file2 = fopen("tempfile.txt", "rb");
+
+                while (fread(&item, sizeof(item), 1, file2)) {
+                    fwrite(&item, sizeof(item), 1, file1);
+                }
+
+                fclose(file1);
+                fclose(file2);
+            }
+
+            printf("\nPress 1 to update or delete another product, or any other key to go back to the main menu.\n");
+            int retryChoice;
+            scanf("%d", &retryChoice);
+            if (retryChoice != 1) {
+                return;
+            }
+        }
+    } while (1);
+}
+
 // EXIT SOFTWARE
 void exitapp() {
     char choice;
@@ -166,7 +316,6 @@ void exitapp() {
         void options();
     }
 }
-
 
 // . . .
 void options();
@@ -226,6 +375,8 @@ void options() {
             addItem();
         } else if (choice == 2) {
             display();
+        } else if (choice == 3) {
+            update();
         } else if (choice == 4) {
             exitapp();
         } else {
